eb2c9d508f5d659f8702c4558065e810319302c8
Lexan4uk <alkatras46rus@gmail.com>
2026-02-02 17:06:10 +0300

    build-fix

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ import gulp from 'gulp'
 import fileInclude from 'gulp-file-include'
 import gulpIf from 'gulp-if'
 import plumber from 'gulp-plumber'
+import replace from 'gulp-replace'
 
 import fonter from 'gulp-fonter'
 import htmlMin from 'gulp-htmlmin'
@@ -20,22 +21,26 @@ import imagemin from 'gulp-imagemin'
 import postCss from 'gulp-postcss'
 import webp from 'gulp-webp'
 import webpCss from 'gulp-webpcss'
-import { createRequire } from 'module'
 import csso from 'postcss-csso'
 import pimport from 'postcss-import'
 import minmax from 'postcss-media-minmax'
 
 import babel from 'gulp-babel'
 import terser from 'gulp-terser'
+import { createRequire } from 'module'
 
 let isBuildFlag = false
+
 const require = createRequire(import.meta.url)
 const bsUtils = require('browser-sync/dist/utils')
 bsUtils.getPorts = (options, cb) => cb(null, options.get('port'))
 bsUtils.getPort = (host, port, max, cb) => cb(null, port)
+
 const sass = gulpSass(dartSass)
+
 const projectFolder = 'prod'
 const sourceFolder = 'src'
+
 const path = {
 	build: {
 		html: projectFolder + '/',
@@ -43,14 +48,16 @@ const path = {
 		js: projectFolder + '/scripts/',
 		img: projectFolder + '/images/',
 		fonts: projectFolder + '/fonts/',
+		svg: projectFolder + '/images/svg/'
 	},
 
 	src: {
-		html: [sourceFolder + '/pages/**/*.html'],
+		html: sourceFolder + '/pages/**/*.html',
 		css: sourceFolder + '/styles/styles.{scss,css}',
 		js: sourceFolder + '/scripts/script.js',
 		img: sourceFolder + '/images/**/*.{jpg,jpeg,png,gif,ico,webp,svg}',
 		fonts: sourceFolder + '/fonts/*.{ttf,eot,otf,ttc,woff,woff2}',
+		svgSprite: sourceFolder + '/images/svg/svgs.svg'
 	},
 
 	watch: {
@@ -58,9 +65,10 @@ const path = {
 		css: sourceFolder + '/styles/**/*.{scss,css}',
 		js: sourceFolder + '/scripts/**/*.js',
 		img: sourceFolder + '/images/**/*.{jpg,jpeg,png,gif,ico,webp,svg}',
+		svgSprite: sourceFolder + '/images/svg/svgs.svg'
 	},
 
-	clean: './' + projectFolder + '/',
+	clean: './' + projectFolder + '/'
 }
 
 export const browserSync = () => {
@@ -69,8 +77,8 @@ export const browserSync = () => {
 		notify: false,
 		browser: 'chrome',
 		server: {
-			baseDir: './' + projectFolder + '/',
-		},
+			baseDir: './' + projectFolder + '/'
+		}
 	})
 }
 
@@ -80,16 +88,9 @@ export const html = () => {
 		.pipe(plumber())
 		.pipe(fileInclude({ basepath: 'src/' }))
 		.pipe(webpHtml())
+		.pipe(gulpIf(isBuildFlag, replace(/(href|src|srcset)="\/(?!\/)/g, '$1="./')))
 		.pipe(gulpIf(isBuildFlag, removeHtml()))
-		.pipe(
-			gulpIf(
-				isBuildFlag,
-				htmlMin({
-					removeComments: true,
-					collapseWhitespace: true,
-				}),
-			),
-		)
+		.pipe(gulpIf(isBuildFlag, htmlMin({ collapseWhitespace: true, removeComments: true })))
 		.pipe(gulp.dest(path.build.html))
 		.pipe(sync.stream())
 }
@@ -98,17 +99,8 @@ export const css = () => {
 	return gulp
 		.src(path.src.css, { sourcemaps: !isBuildFlag })
 		.pipe(bulk())
-		.pipe(
-			sass({
-				outputStyle: 'expanded',
-			}),
-		)
-		.pipe(
-			webpCss({
-				webpClass: '.webp',
-				noWebpClass: '.no-webp',
-			}),
-		)
+		.pipe(sass({ outputStyle: 'expanded' }))
+		.pipe(webpCss({ webpClass: '.webp', noWebpClass: '.no-webp' }))
 		.pipe(postCss([pimport, autoprefixer, minmax]))
 		.pipe(gulpIf(isBuildFlag, postCss([csso])))
 		.pipe(gulp.dest(path.build.css, { sourcemaps: !isBuildFlag }))
@@ -119,14 +111,7 @@ export const js = () => {
 	return gulp
 		.src(path.src.js, { sourcemaps: !isBuildFlag })
 		.pipe(fileInclude())
-		.pipe(
-			gulpIf(
-				isBuildFlag,
-				babel({
-					presets: ['@babel/preset-env'],
-				}),
-			),
-		)
+		.pipe(gulpIf(isBuildFlag, babel({ presets: ['@babel/preset-env'] })))
 		.pipe(gulpIf(isBuildFlag, terser()))
 		.pipe(gulp.dest(path.build.js, { sourcemaps: !isBuildFlag }))
 		.pipe(sync.stream())
@@ -134,59 +119,45 @@ export const js = () => {
 
 export const images = () => {
 	return gulp
-		.src(path.src.img)
+		.src([path.src.img, '!' + path.src.svgSprite])
 		.pipe(webp())
 		.pipe(gulp.dest(path.build.img))
-		.pipe(gulp.src(path.src.img))
-		.pipe(
-			gulpIf(
-				isBuildFlag,
-				imagemin({
-					progressive: true,
-					verbose: true,
-					optimizationLevel: 3,
-				}),
-			),
-		)
+		.pipe(gulp.src([path.src.img, '!' + path.src.svgSprite]))
+		.pipe(gulpIf(isBuildFlag, imagemin({ optimizationLevel: 3, progressive: true })))
 		.pipe(gulp.dest(path.build.img))
+}
+
+export const svgSprite = () => {
+	return gulp.src(path.src.svgSprite).pipe(plumber()).pipe(gulp.dest(path.build.svg)).pipe(sync.stream())
 }
 
 export const fonts = () => {
 	return gulp
 		.src(path.src.fonts)
 		.pipe(plumber())
-		.pipe(
-			fonter({
-				formats: ['ttf', 'eot', 'woff', 'svg'],
-			}),
-		)
+		.pipe(fonter({ formats: ['ttf', 'eot', 'woff', 'svg'] }))
 		.pipe(gulp.dest(path.build.fonts))
 		.pipe(ttf2woff2())
 		.pipe(gulp.dest(path.build.fonts))
 }
 
-export const clean = () => {
-	return del(path.clean)
-}
+export const clean = () => del(path.clean)
 
-const setMode = isBuild => {
-	return cb => {
-		isBuildFlag = isBuild
-		cb()
-	}
+const setMode = isBuild => cb => {
+	isBuildFlag = isBuild
+	cb()
 }
 
 export const watchFiles = () => {
-	gulp.watch([path.watch.html], html)
-	gulp.watch([path.watch.css], css)
-	gulp.watch([path.watch.js], js)
-	gulp.watch([path.watch.img], images)
+	gulp.watch(path.watch.html, html)
+	gulp.watch(path.watch.css, css)
+	gulp.watch(path.watch.js, js)
+	gulp.watch(path.watch.img, images)
+	gulp.watch(path.watch.svgSprite, svgSprite)
 }
 
+const dev = gulp.parallel(css, html, js, images, svgSprite)
 const watch = gulp.parallel(watchFiles, browserSync)
 
-const dev = gulp.parallel(css, html, js, images)
-
 export const build = gulp.series(clean, setMode(true), dev, fonts, browserSync)
-
 export default gulp.series(clean, dev, watch)
